7a055840f8190da669072cbcba90bd09fa6d63dd
Mateusz Wójcik <mwojci@student.agh.edu.pl>
2024-05-04 13:57:20 +0200

    S1: Back-end : 1 : Stworzenie klasy kart - dodanie testów jednostkowych i przetestowanie tworzenia oraz kopiowania instancji

--- test/test_classes.cpp
+++ test/test_classes.cpp
@@ -4,5 +4,18 @@
 
 TEST(CardTest, constructor)
 {
-    Card(hearts, ten);
+    Card card(Card_Color_t::hearts, Card_Value_t::ten);
+
+    EXPECT_EQ(Card_Color_t::hearts, card.getCardColor());
+    EXPECT_EQ(Card_Value_t::ten, card.getCardValue());
+}
+
+TEST(CardTest, copy_constructor)
+{
+    Card card1(Card_Color_t::hearts, Card_Value_t::ten);
+    Card card2(Card_Color_t::hearts, Card_Value_t::ten);
+
+    EXPECT_EQ(Card_Color_t::hearts, card2.getCardColor());
+    EXPECT_EQ(Card_Value_t::ten, card2.getCardValue());
+    EXPECT_EQ(card1.getCard(), card2.getCard());    
 }
