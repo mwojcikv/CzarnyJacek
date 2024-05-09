3979fc31170beb4215b8ca30a7992be19788acf4
Maksimo213 <167205401+Maksimo213@users.noreply.github.com>
2024-05-09 21:58:50 +0200

    GUI: 2: stworzenie dokumentacji używanych bibliotek (instrukcja jak pobrac uzywaną bibliotekę Openc)
    Stworzenie konceptu przedstawienia instalacji bibliotek potrzebnych do prawidłowego działania kodu

--- src/gui/commandLineInterface.cpp
+++ src/gui/commandLineInterface.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
 #include <vector>
-#include <opencv2/opencv.hpp>
+#include <opencv4/opencv2/opencv.hpp>
 
 using namespace std;
 using namespace cv;
 
-int wczytaj_kartę() {
+int card_loading() {
     // Lista nazw plików z kartami
   // Tutaj będą dodawane karty z tali
     vector<string> fileNames = {"karta1.jpg", "karta2.jpg", "karta3.jpg"}; 
@@ -33,4 +33,5 @@ int wczytaj_kartę() {
     return 0;
 }
 
-// kod jest narazie szkieletem orginalnej wersji ale mniej więcej na tym będzie polegała inplementacja kart i wyswietlanie je w formacie .jpg
+//bedzie tu realizowana ,,GUI: 2: stworzenie dokumentacji używanych bibliotek (instrukcja jak pobrac uzywaną bibliotekę Openc)"
+// mój koncept na to zadanie opiera sie na opisaniu i ułatwieniu poprawnej intalacji biblioteki.
