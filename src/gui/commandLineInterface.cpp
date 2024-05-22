574981abd34bf08d0a5b98aafcaa21fd6b069113
Mikolaj Wandzel <wandzelm04@gmail.com>
2024-05-22 13:16:54 +0200

    Dodanie czcionki arial, oraz wprowadzenie funkcji wyświetlania graficznego interfejsu, pojawia się błąd którego na tem moment nie potrafię rozwiązać

--- src/gui/commandLineInterface.cpp
+++ src/gui/commandLineInterface.cpp
@@ -1,74 +1,75 @@
 #include "gui/commandLineInterface.hpp"
-#include <iostream>
 #include <vector>
 #include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/System.hpp>
+#include <iostream>
 
 
-void interface() {
-    int opcja;
-    std::cout << "\n\n========================\n" << "===== Czarny Jacek =====\n" << "========================\n\n";
-    std::cout << "Menu:\n";
-    std::cout << "1. Rozgrywka z botami\n";
-    std::cout << "2. Rozgrywka multiplayer\n";
-    std::cout << "3. Sklep\n";
-    std::cout << "4. Ekskluzywna zawartosc dla uzytkownikow\n";
-    std::cout << "5. Zasady gry w Blackjack\n";
-    std::cout << "6. Wyjscie\n\n";
-    std::cout << "Wybierz opcje: ";
-    std::cin >> opcja;
-    //system("cls");
-
-    switch (opcja) {
-        case 1:
-            std::cout << "\n===== Rozgrywka z botami =====\n\n";
-            std::cout << "*gui_dla_botow*\n";
-            std::cout << "1. Rozpocznij gre\n";
-            std::cout << "2. Wybierz poziom trudnosci\n";
-            std::cout << "3. Wroc do menu\n\n";
-            int opcja1;
-            std::cout << "Wybierz opcje: ";
-            std::cin >> opcja1;
-            //system("cls");
-            break;
-        case 2:
-            std::cout << "\n===== Rozgrywka multiplayer =====\n\n";
-            std::cout << "*gui_dla_multiplayera*\n";
-            std::cout << "1. Rozpocznij gre\n";
-            std::cout << "2. Twoja ranga\n";
-            std::cout << "3. Wroc do menu\n\n";
-            int opcja2;
-            std::cout << "Wybierz opcje: ";
-            std::cin >> opcja2;
-            //system("cls");
-            break;
-        case 3:
-            std::cout << "\n===== Sklep =====\n\n";
-            std::cout << "*gui_dla_sklepu*\n";
-            std::cout << "1. Sklep z kartami\n";
-            std::cout << "2. Sklep z ulepszeniami\n";
-            std::cout << "3. Wroc do menu\n\n";
-            int opcja3;
-            std::cout << "Wybierz opcje: ";
-            std::cin >> opcja3;
-            //system("cls");
-            break;
-        case 4:
-            std::cout << "\n===== Ekskluzywna zawartosc dla uzytkownikow =====\n\n";
-            std::cout << "*gui_dla_zawartosci*\n";
-            break;
-        case 5:
-            std::cout << "\n===== Zasady gry w Blackjack =====\n\n";
-            std::cout << "*gui_dla_zasad*\n";
-            break;
-        case 6:
-            std::cout << "\nDziekujemy za gre, do nastepnego!\n\n";
-            std::cout << "*exit_game*\n";
-            break;
-        default:
-            std::cout << "\nNieprawidlowy wybor. Wybierz ponownie.\n";
-            break;
-    }
-}
+//funkcja nie działa przez problem sfml
+//void Create_Window()
+//{
+//    // Utworzenie okna
+//    sf::RenderWindow window(sf::VideoMode(800, 600), "SFML Window");
+//
+//    // Ustawienie zielonego tła
+//    sf::Color backgroundColor = sf::Color::Green;
+//
+//    // Czcionka i napis
+//    sf::Font font;
+//    if (!font.loadFromFile("C:/Users/PC/Desktop/Blackjack/CzarnyJacek/arial.ttf")) {
+//        std::cerr << "Failed to load font \"arial.ttf\"" << std::endl;
+//    }
+//
+//    sf::Text title("czarnyjacek", font, 24);
+//    title.setFillColor(sf::Color::Black);
+//    title.setPosition(350, 20); // Ustawienie pozycji napisu
+//
+//    // Przyciski
+//    sf::RectangleShape startButton(sf::Vector2f(100, 50));
+//    startButton.setFillColor(sf::Color::White);
+//    startButton.setPosition(50, 100);
+//
+//    sf::Text startText("Start", font, 20);
+//    startText.setFillColor(sf::Color::Black);
+//    startText.setPosition(70, 110); // Ustawienie pozycji napisu na przycisku
+//
+//    sf::RectangleShape exitButton(sf::Vector2f(100, 50));
+//    exitButton.setFillColor(sf::Color::White);
+//    exitButton.setPosition(50, 200);
+//
+//    sf::Text exitText("Wyjscie", font, 20);
+//    exitText.setFillColor(sf::Color::Black);
+//    exitText.setPosition(60, 210); // Ustawienie pozycji napisu na przycisku
+//
+//    while (window.isOpen()) {
+//        sf::Event event;
+//        while (window.pollEvent(event)) {
+//            if (event.type == sf::Event::Closed) {
+//                window.close();
+//            } else if (event.type == sf::Event::MouseButtonPressed) {
+//                sf::Vector2i mousePos = sf::Mouse::getPosition(window);
+//                sf::Vector2f mousePosF(static_cast<float>(mousePos.x), static_cast<float>(mousePos.y));
+//                if (startButton.getGlobalBounds().contains(mousePosF)) {
+//                    // Kod, który ma się wykonać po kliknięciu przycisku "Start"
+//                    std::cout << "Start button clicked" << std::endl;
+//                } else if (exitButton.getGlobalBounds().contains(mousePosF)) {
+//                    window.close(); // Zamykanie okna po kliknięciu "Wyjście"
+//                }
+//            }
+//        }
+//
+//        window.clear(backgroundColor);
+//
+//        window.draw(title);
+//        window.draw(startButton);
+//        window.draw(startText);
+//        window.draw(exitButton);
+//        window.draw(exitText);
+//
+//        window.display();
+//    }
+//}
 
 void displayDeck(std::string suit, std::string rank) {
             std::string imagePath = "C:/Users/PC/Desktop/Blackjack/CzarnyJacek/cards/" + rank + "_of_" + suit + ".png";
@@ -103,5 +104,3 @@ void displayDeck(std::string suit, std::string rank) {
 //         window.display();
 //     }
 // }
-
-
